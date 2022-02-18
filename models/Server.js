42da6e2f79ebd7c84d6358c5d0b227951ef303bc
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-02-18 20:55:44 -0300

    modelado del server

--- models/Server.js
+++ models/Server.js
@@ -1,6 +1,23 @@
 const express = require("express");
- 
+const cors = require("cors");
+
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 8080;
+  }
 
+  middlewares() {
+    //cors
+    this.app.use(cors());
 
+    //parseo del body y lectura
+    this.app.use(express.json());
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto ", this.port);
+    });
+  }
 }

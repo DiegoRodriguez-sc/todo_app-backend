dd80e6274a6038b1000fd402b985b37376ad12fc
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-02-19 15:56:30 -0300

    get all users config

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,31 @@
 const { request, response } = require("express");
+const User = require("../models/user");
+
+//Get all users
+const getUsers = async (req = request, res = response) => {
+  try {
+    const usersDB = await User.find({ state: true });
+    const contUsers = await User.countDocuments({ state: true });
+    res.status(200).json({
+      error: false,
+      total: contUsers,
+      users: usersDB,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: 500,
+      msg: "Hable con el administrador",
+      path: "/api/user",
+    });
+  }
+};
 
 const getUserById = async (req = request, res = response) => {
   res.json({ msg: "Hola" });
 };
 
 module.exports = {
+  getUsers,
   getUserById,
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { getUserById } = require("../controllers/user.controller");
+const { getUserById, getUsers } = require("../controllers/user.controller");
 
 const router = Router();
 
+router.get("/", getUsers);
 router.get("/:id", getUserById);
 
 module.exports = router;

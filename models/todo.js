baddcd432d37379fd6c696994891baf56ed9f3ee
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-02-19 18:59:48 -0300

    model todo added

---
+++ models/todo.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const TodoSchema = Schema(
+  {
+    bodyTodo: {
+      type: String,
+      required: [true, "Texto del todo requerido"],
+    },
+    status: {
+      type: String,
+      enum: ["completed", "incomplete"],
+      default: "incomplete",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+TodoSchema.methods.toJSON = function () {
+  const { __v, _id, ...todo } = this.toObject();
+  todo.uid = _id;
+  return todo;
+};
+
+module.exports = model("Todo", TodoSchema);
